b22c4c05e25a6257187905a789a1c0af80990eba
kumar <dh26kumar@gmail.com>
2026-08-06 14:11:26 +0530

    test uodated 2nd time

--- selenium_test.py
+++ selenium_test.py
@@ -2,16 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
+
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Remote(
-    command_executor="http://127.0.0.1:4444/wd/hub",
+    command_executor="http://selenium-chrome:4444",
     options=options
 )
 
-driver.get("http://localhost:5001")
+driver.get("http://myapp-container:5000")
 
 driver.find_element(By.NAME, "username").send_keys("admin")
 driver.find_element(By.NAME, "password").send_keys("admin")
