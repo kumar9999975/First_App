eb2749e98d7102c51fb439110043216b2d80324d
kumar <dh26kumar@gmail.com>
2026-08-10 13:49:59 +0530

    test file changed

--- selenium_test.py
+++ selenium_test.py
@@ -1,24 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
+driver = webdriver.Chrome()
 
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+driver.get("http://localhost:5000")
 
-driver = webdriver.Remote(
-    command_executor="http://selenium-chrome:4444",
-    options=options
-)
-
-driver.get("http://myapp-container:5000")
-
+# Username
 driver.find_element(By.NAME, "username").send_keys("admin")
+
+# Password
 driver.find_element(By.NAME, "password").send_keys("admin")
 
+# Login button
 driver.find_element(By.ID, "login").click()
-
-print("Test completed")
 
 driver.quit()
